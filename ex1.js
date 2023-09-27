050fc50988e78189d468b7e98ba91de24c787dcc
LETICIA EVELYN DE SOUZA LUBRIGATE <leticia.eslubrigate@senacsp.edu.br>
2023-09-27 16:40:34 -0300

    first commit

---
+++ ex1.js
@@ -0,0 +1,41 @@
+/*
+** EXERCICIO 1 **
+
+
+let n1 = prompt("Olá, digite um número: ")
+let n2 = prompt("Digite outro número:")
+
+let resultado = parseInt(n1) + parseInt(n2)
+
+alert(resultado)
+*/
+
+
+
+/* let base = prompt("Digite a Base")
+let altura = prompt("Digite a Altura:")
+let resposta = (parseFloat(base)*parseFloat(altura))/2
+*/
+
+/* let n1 = prompt("Digite a sua primeira nota")
+let n2 = prompt("Digite a sua segunda nota")
+let n3 = prompt("Digite a sua terceira nota")
+let resposta = (parseFloat(n1) + parseFloat (n2) + parseFloat (n3))/3
+alert("a sua nota foi de" +Math. round(resposta))
+*/
+
+/*
+let matricula = prompt("Qual o seu número de matrícula")
+let carga = prompt("Digite as horas trabalhadas")
+let salario = prompt("Valor que recebe por hora")
+let resultado = (carga*salario)
+alert("seu codigo de funcionario é"+ matricula +"seu salário é de:" +resultado)
+*/
+
+let distancia = prompt("Digite a distancia total percorrida")
+let consumo = prompt("Digite o quanto o automovel consumiu")
+let resultado = (distancia/consumo)
+alert("O consumo médio é de" +resultado.toFixed(3) +"km/l")
+
+
+
---
+++ script.js
@@ -0,0 +1,13 @@
+/* let nome = "Thais" // string
+let idade = 33 // inteiro
+let sexo = "Feminino" // double
+let altura = 1.75 // double
+let temGato = false // booleano */
+
+//alert("Oi meu nome é "+nome+" e eu tenho "+idade+" anos")
+//alert(`Oi meu nome é ${nome} e eu tenho ${idade} anos`)
+
+//console.log("Oi meu nome é "+nome+" e eu tenho "+idade+" anos")
+
+//let nome = prompt("Olá, qual seu nome?")
+//alert("Seja bem vindo "+nome)
